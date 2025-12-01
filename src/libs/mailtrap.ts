dc407011f5c1bdf42cb3adee86d7c08655f5d927
samuel alves de souza <samuel.alvessd@gmail.com>
2025-12-01 16:32:31 -0300

    fix mailtrap version for test

--- src/libs/mailtrap.ts
+++ src/libs/mailtrap.ts
@@ -1,21 +1,23 @@
+import { id } from 'zod/v4/locales';
 import { MailtrapClient } from "mailtrap"
 
 export const sendEmail = async (to: string, subject: string, body: string) => {
 
     const mailtrap = new MailtrapClient({
         token: process.env.MAILTRAP_TOKEN as string,
-        testInboxId: 4223375
+        testInboxId: 4223538
     })
 
     try {
-        await mailtrap.send({
+        await mailtrap.testing.send({
             from: { name: 'Sistema', email: 'sitema@google.com.br' },
             to: [{ email: to }],
             subject,
-            text: body
+            text: body,
         });
         return true;
     } catch (err) {
         return false;
     }
+
 }
